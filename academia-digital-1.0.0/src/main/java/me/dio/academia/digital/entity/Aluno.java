de7350a4b0de9a103c8a26168a23516dfa9538cc
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-04-18 20:11:42 -0300

    Criando as anotações

--- academia-digital-1.0.0/src/main/java/me/dio/academia/digital/entity/Aluno.java
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -1,15 +1,28 @@
 package me.dio.academia.digital.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_alunos")
 public class Aluno {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String nome;
 
+  @Column(unique = true)
   private String cpf;
 
   private String bairro;
