42ebbaf3c178d8d3d4a8ba56723cd15530f2a31c
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-04-18 20:24:28 -0300

    criando anotações em Avaliaçoes

--- academia-digital-1.0.0/src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 
@@ -16,6 +15,9 @@ import java.time.LocalDateTime;
 @Table(name = "tb_avaliacoes")
 public class AvaliacaoFisica {
 
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private Aluno aluno;
