f8ad4c59c36f4e9a8582283db292887ca9c6c2b1
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-04-18 20:30:39 -0300

    criando notação/tabela em av

--- academia-digital-1.0.0/src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -20,12 +20,16 @@ public class AvaliacaoFisica {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "aluno_id")
   private Aluno aluno;
 
   private LocalDateTime dataDaAvaliacao = LocalDateTime.now();
 
+  @Column(name = "peso_atual")
   private double peso;
 
+  @Column(name = "altura_atual")
   private double altura;
 
 }
