745eddfd35ddce35cb1ba339c1bdbd9092a31292
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-04-19 22:37:02 -0300

    CRUD pt6

--- academia-digital-1.0.0/src/main/java/me/dio/academia/digital/service/impl/AvaliacaoFisicaServiceImpl.java
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/service/impl/AvaliacaoFisicaServiceImpl.java
@@ -3,7 +3,9 @@ package me.dio.academia.digital.service.impl;
 import me.dio.academia.digital.entity.AvaliacaoFisica;
 import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
 import me.dio.academia.digital.entity.form.AvaliacaoFisicaUpdateForm;
+import me.dio.academia.digital.repository.AvaliacaoFisicaRepository;
 import me.dio.academia.digital.service.IAvaliacaoFisicaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,8 +13,12 @@ import java.util.List;
 @Service
 public class AvaliacaoFisicaServiceImpl implements IAvaliacaoFisicaService {
 
+    @Autowired
+    private AvaliacaoFisicaRepository repository;
+
     @Override
     public AvaliacaoFisica create(AvaliacaoFisicaForm form) {
+        AvaliacaoFisica avaliacaoFisica = new AvaliacaoFisica();
         return null;
     }
 
