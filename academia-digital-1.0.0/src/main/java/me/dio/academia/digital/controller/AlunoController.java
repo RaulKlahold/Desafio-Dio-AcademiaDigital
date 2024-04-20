0708fcdf8a74b77c31b33fda3075fc4cbedf8876
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-04-19 21:58:45 -0300

    crudo pt2

--- academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -1,6 +1,7 @@
 package me.dio.academia.digital.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,10 @@ public class AlunoController {
 
     @Autowired
     private AlunoServiceImpl service;
+
+    @GetMapping
+    public List<Aluno> getAll(){
+        return service.getAll();
+    }
+
 }
--- academia-digital-1.0.0/src/main/java/me/dio/academia/digital/service/impl/AlunoServiceImpl.java
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/service/impl/AlunoServiceImpl.java
@@ -4,7 +4,9 @@ package me.dio.academia.digital.service.impl;
 import me.dio.academia.digital.entity.Aluno;
 import me.dio.academia.digital.entity.form.AlunoForm;
 import me.dio.academia.digital.entity.form.AlunoUpdateForm;
+import me.dio.academia.digital.repository.AlunoRepository;
 import me.dio.academia.digital.service.IAlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +18,8 @@ public class AlunoServiceImpl implements IAlunoService {
         return null;
     }
 
+    @Autowired
+    private AlunoRepository repository;
     @Override
     public Aluno get(Long id) {
         return null;
